0270cd78e92640f34f5e4f9380e90d549422ba57
kajasharif31 <kajasharif366@gmail.com>
2024-02-28 00:22:20 +0530

    fix: login & creat user

--- src/pages/AuthCreate.jsx
+++ src/pages/AuthCreate.jsx
@@ -3,35 +3,6 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase/index";
 
 const AuthCreate = () => {
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [user, setUser] = useState({});
-
-  const register = async (e) => {
-    e.preventDefault();
-    try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        registerEmail,
-        registerPassword
-      );
-      const newUser = userCredential.user;
-      console.log(newUser);
-      // Redirect to Home after successful registration
-      window.location.href = "/";
-      alert("Vault Created Successfully😊");
-    } catch (error) {
-      console.log(error.message);
-      if (error.code === "auth/invalid-credential") {
-        alert("Invalid credentials");
-      } else if (error.code === "auth/invalid-email") {
-        alert("Enter a valid email");
-      } else if (error.code === "auth/weak-password") {
-        alert("Password should be a minimum of 6 characters");
-      }
-    }
-  };
-
   return (
     <div>
       <section className="bg-gray-50 dark:bg-gray-900">
@@ -66,67 +37,6 @@ const AuthCreate = () => {
                 />
               </svg>
             </a>
-          </div>
-          <div>
-            {/* login */}
-            <div className="w-full lg:max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow-xl dark:bg-gray-800">
-              <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
-                Welcome to DesireVault!{" "}
-                {/* <span className="text-blue-500">{user?.email}</span> */}
-              </h2>
-              <h2 className="text-xl font-bold text-blue-500 dark:text-blue-500 ">
-                Create Your account to continue
-              </h2>
-
-              <form className="mt-8 space-y-6" onSubmit={register}>
-                {/* register */}
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Your email
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@company.com"
-                    required
-                    onChange={(event) => {
-                      setRegisterEmail(event.target.value);
-                    }}
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Your password
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required
-                    onChange={(event) => {
-                      setRegisterPassword(event.target.value);
-                    }}
-                  />
-                </div>
-                <button
-                  type="submit"
-                  className="w-full px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  onClick={register}
-                >
-                  Create my account
-                </button>
-              </form>
-            </div>
           </div>
         </div>
       </section>
